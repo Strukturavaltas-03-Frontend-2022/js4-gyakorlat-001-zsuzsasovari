f7f9a8f5f903a6a0f3f4ddd29d6ad9d52fc75759
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-09-05 22:19:58 +0200

    kész

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,18 @@
+// 1. Írj egy függvényt, `convertToUppercase()` néven, mely paraméterként kap egy tömböt. A függvény visszatérési érték egy Promise.  
+// Amennyiben a tömb minden eleme string, és sikeresen nagybetűssé alakíthatók a Promise resolved lesz és visszaad egy új tömböt, 
+//ami az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza.  
+// Ellenkező esetben a Promise rejected lesz, a visszatérési érték a következő string: 'Error: Not all items in the array are strings!'
+const convertToUppercase = (input) => {
+    return new Promise((resolve, reject) => {
+        let result = [];
+        input.forEach(element => {
+            if(typeof element !== 'string'){
+                reject(new Error('Error: Not all items in the array are strings!'));
+            } else {
+                result.push(element.toLocaleUpperCase());
+            }
+    });
+    resolve(result);
+})};
+
 export default convertToUppercase;
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,22 @@
+// 2. Írj egy másik függvényt `sortStrings()` néven, mely paraméterként egy tömböt kap. A függvény visszatérési érték egy Promise.  
+// A Promise resolved lesz, amennyiben a tömb minden eleme string, a resolved value az ABC szerinti rendezett tömb.  
+// Ha nem minden elem string, akkor a Promise rejected lesz, az Error objektum üzenet szövege:     
+// 'Error: Not all items in the array are strings!'   
+// Ha a sorba rendezés során hiba történik akkor is rejected lesz a Promise, az Error objektum üzenet szövege ebben az esetben:  
+// 'Error: Something went wrong with sorting words.'
+const sortStrings = (input) => {
+    return new Promise((resolve, reject) =>{
+        if(!Array.isArray(input)) {
+            reject(new Error('Error: Something went wrong with sorting words!'));
+        }
+
+        input.forEach(element => {
+            if(typeof element !== 'string'){
+                reject(new Error('Error: Not all items in the array are strings!'));
+            }
+        });
+        resolve(input.sort());
+    });
+}
+
 export default sortStrings;
